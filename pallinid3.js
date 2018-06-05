60f426e53f906af8fad8827984158488114540f2
Alessandro Zotta <zotta.ale@gmail.com>
2018-06-06 00:19:35 +0200

    fixed margins

--- pallinid3.js
+++ pallinid3.js
@@ -9,8 +9,8 @@ let svg = d3.selectAll("body")
 .attr("height", height - margin)
 .style("background", "#191919")
 
-let randomX = d3.randomUniform(innerMargin + margin, width - margin - innerMargin*2)
-let randomY = d3.randomUniform(innerMargin + margin, height - margin - innerMargin*2)
+let randomX = d3.randomUniform(innerMargin, width - margin - innerMargin*2)
+let randomY = d3.randomUniform(innerMargin, height - margin - innerMargin*2)
 
 let pallini = 29148
 
